fa5f36793e6da6a4895a8aceae0e7df622e90064
SPandey <shikhapandey95@gmail.com>
2022-05-15 09:27:02 +0530

    Code first checkin

---
+++ Controllers/UserController.cs
@@ -0,0 +1,52 @@
+﻿using JWTApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Security.Claims;
+
+namespace JWTApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        [HttpGet("Admins")]
+        [Authorize(Roles = "Administrator")]
+        public IActionResult AdminsEndpoint()
+        {
+            var currentUser = GetCurrentUser();
+
+            return Ok($"Hi {currentUser.GivenName}, you are an {currentUser.Role}");
+        }
+
+
+        [HttpGet("Public")]
+        public IActionResult Public() 
+        {
+            return Ok("Hi! You are on public property");
+        }
+
+        private UserModel GetCurrentUser()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+
+            if (identity != null)
+            {
+                var userClaims = identity.Claims;
+
+                return new UserModel
+                {
+                    Username = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.NameIdentifier)?.Value,
+                    EmailAddress = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Email)?.Value,
+                    GivenName = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.GivenName)?.Value,
+                    Surname = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Surname)?.Value,
+                    Role = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Role)?.Value
+                };
+            }
+            return null;
+        }
+
+    }
+}
---
+++ Models/UserConstants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTApp.Models
+{
+    public class UserConstants
+    {
+        public static List<UserModel> Users = new List<UserModel>()
+        {
+            new UserModel() { Username = "shikha_admin", EmailAddress = "shikha.admin@email.com", 
+                Password = "MyPass_w0rd", GivenName = "Shikha", Surname = "Pandey", 
+                Role = "Administrator" },
+            new UserModel() { Username = "elyse_seller", EmailAddress = "elyse.seller@email.com", 
+                Password = "MyPass_w0rd", GivenName = "Elyse", Surname = "Lambert", 
+                Role = "Seller" },
+        };
+    }
+}
